d77ccb2c069581e9ebdb8ab0afb27ca6d766792c
Chris <hearmewhisper@gmail.com>
2020-07-29 14:54:10 -0400

    testing, not working...

--- client-react/src/App.js
+++ client-react/src/App.js
@@ -15,10 +15,10 @@ function App() {
           <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
             <ul className="navbar-nav ml-auto">
               <li className="nav-item">
-                <Link className="nav-link" to={"/sign-in"}>Login</Link>
+                <Link className="nav-link" to={"/login"}>Login</Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" to={"/sign-up"}>Sign up</Link>
+                <Link className="nav-link" to={"/signup"}>Sign up</Link>
               </li>
               <li className="nav-item">
                 <Link className="nav-link" to={"/home"}>Home</Link>
@@ -32,10 +32,10 @@ function App() {
         <div className="auth-inner">
           <Switch>
             <Route exact path='/' component={Login} />
-            <Route path="/sign-in" component={Login} />
-            <Route path="/sign-up" component={SignUp} />
-            <Route path="/tasks" component={Task}/>
-            <Route path="/home" component={Home}/>
+            <Route path="/login" component={Login} />
+            <Route path="/signup" component={SignUp} />
+            <Route path="/tasks" component={Task} />
+            <Route path="/home" component={Home} />
           </Switch>
         </div>
       </div>
--- client-react/src/components/signup.component.js
+++ client-react/src/components/signup.component.js
@@ -1,29 +1,53 @@
 import React, { Component } from "react";
+import axios from "axios";
+
+
 
 export default class SignUp extends Component {
+
+    addUser = () => {
+        let url = "http://localhost:3001/users/signup";
+        axios.post(url, {
+            firstName: this.firstName.current.value,
+            lastName: this.lastName.current.value,
+            email: this.email.current.value,
+            username: this.username.current.value,
+            password: this.password.current.value
+        }).then(response => {
+            // refresh the data
+            console.log(response);
+            // empty the input
+            // this.users.current.value = "";
+        });
+    };
     render() {
         return (
-            <form>
+            <form onSubmit={this.addUser}>
                 <h3>Sign Up</h3>
 
                 <div className="form-group">
                     <label>First name</label>
-                    <input type="text" className="form-control" placeholder="First name" />
+                    <input type="text" name="firstName" className="form-control" placeholder="First name" />
                 </div>
 
                 <div className="form-group">
                     <label>Last name</label>
-                    <input type="text" className="form-control" placeholder="Last name" />
+                    <input type="text" name="lastName" className="form-control" placeholder="Last name" />
                 </div>
 
                 <div className="form-group">
                     <label>Email address</label>
-                    <input type="email" className="form-control" placeholder="Enter email" />
+                    <input type="email" name="email" className="form-control" placeholder="Enter email" />
+                </div>
+
+                <div className="form-group">
+                    <label>Username</label>
+                    <input type="text" name="username" className="form-control" placeholder="Enter username" />
                 </div>
 
                 <div className="form-group">
                     <label>Password</label>
-                    <input type="password" className="form-control" placeholder="Enter password" />
+                    <input type="password" name="password" className="form-control" placeholder="Enter password" />
                 </div>
 
                 <button type="submit" className="btn btn-primary btn-block">Sign Up</button>
